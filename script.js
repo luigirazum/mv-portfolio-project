7516339c1d2d59b2ea4fd7d69fb8ff9e61acfea9
Juanpa8830 <jp3_4@hotmail.com>
2022-11-14 19:23:48 -0500

    add show/hide feature to mobile_menu

---
+++ script.js
@@ -0,0 +1,33 @@
+const menuButton = document.querySelector('.menu_icon');
+const mobileMenu = document.querySelector('#mobile_menu');
+const closeMenu = document.querySelector('.btn-close');
+const header = document.querySelector('.header');
+const portfolioLink = document.querySelector('.portfolioLink');
+const aboutLink = document.querySelector('.aboutLink');
+const contactLink = document.querySelector('.contactLink');
+
+
+menuButton.addEventListener('click', () => {
+  mobileMenu.classList.replace('hide_menu', 'show_menu');
+  header.classList.replace('show_menu', 'hide_menu');
+});
+
+closeMenu.addEventListener('click', () => {
+  mobileMenu.classList.replace('show_menu', 'hide_menu');
+  header.classList.replace('hide_menu', 'show_menu');
+});
+
+portfolioLink.addEventListener('click', () => {
+  mobileMenu.classList.replace('show_menu', 'hide_menu');
+  header.classList.replace('hide_menu', 'show_menu');
+});
+
+aboutLink.addEventListener('click', () => {
+    mobileMenu.classList.replace('show_menu', 'hide_menu');
+    header.classList.replace('hide_menu', 'show_menu');
+  });
+
+  contactLink.addEventListener('click', () => {
+    mobileMenu.classList.replace('show_menu', 'hide_menu');
+    header.classList.replace('hide_menu', 'show_menu');
+  });
